550c3eefbb8075ca6f56ae6f89f97a1d316ae4c4
Gor1x <ea-post134@yandex.ru>
2019-01-22 09:06:55 +0300

    added save training button

--- app/src/main/java/com/example/fitnes/ExerciseChoosing.java
+++ app/src/main/java/com/example/fitnes/ExerciseChoosing.java
@@ -2,9 +2,14 @@ package com.example.fitnes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import java.util.List;
 
@@ -14,10 +19,17 @@ public class ExerciseChoosing extends AppCompatActivity implements ExerciseAdapt
 
     private List<Exercise> exerciseList;
     private RecyclerView list;
+    private View viewExercise;
+    private Toolbar toolbarExercise;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_choose);
+        viewExercise = findViewById(R.id.coordinator_exercise);
+        toolbarExercise = findViewById(R.id.toolbar_exercise);
+        setSupportActionBar(toolbarExercise);
+
         list = findViewById(R.id.list);
         list.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
@@ -35,4 +47,18 @@ public class ExerciseChoosing extends AppCompatActivity implements ExerciseAdapt
         startActivity(intent);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.save_training) {
+            Snackbar.make(viewExercise, "You have saved training", Snackbar.LENGTH_LONG).show();
+        }
+        return true;
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.choose_exercise_menu, menu);
+        return true;
+    }
 }
